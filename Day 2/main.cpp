71b6dc110f08d01635dbc933b90634a4917f0b1a
TikoTako <49921594+TikoTako@users.noreply.github.com>
2021-12-02 21:13:23 +0100

    v2

--- Day 2/main.cpp
+++ Day 2/main.cpp
@@ -1,103 +1,112 @@
-#include <iostream>
+#include <format>
 #include <vector>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include <iostream>
+
+// std::format <- latest standard 
 
 struct DataStruct
 {
-	std::vector <int> Forwards;
-	std::vector <int> Downs;
-	std::vector <int> Ups;
+	char wut;
+	int wat;
 };
 
-void PartOne()
-{	
+void PartOne(std::vector<DataStruct>* rawData)
+{
 	// example 150
 	// data 1714950
 	int forwards = 0, ups = 0, downs = 0;
-	std::string dataFileName = "data.txt";
-	std::ifstream dataFileStream(dataFileName);
-	if (dataFileStream.is_open())
+
+	for (auto& data : *rawData)
 	{
-		std::cout << "open ok" << std::endl;
-		std::string line;
-		while (dataFileStream.good())
+		switch (data.wut)
 		{
-			std::getline(dataFileStream, line);
-			switch (line[0])
-			{
-				case 'f': // forward
-					forwards += std::atoi(line.substr(7, line.length() - 7).c_str());
-					break;
-				case 'u': // up
-					ups += std::atoi(line.substr(3, line.length() - 3).c_str());
-					break;
-				case 'd': // down
-					downs += std::atoi(line.substr(4, line.length() - 4).c_str());
-					break;
-				default:
-					std::cout << "da fuq ??" << std::endl;
-					break;
-			}
+			case 'f': // forward
+				forwards += data.wat;
+				break;
+			case 'u': // up
+				ups += data.wat;
+				break;
+			case 'd': // down
+				downs += data.wat;
+				break;
+			default:
+				break;
 		}
-		std::cout << forwards << " - " << ups << " " << downs << " " << (downs - ups) * forwards << std::endl;
-		dataFileStream.close();
 	}
-	else
-	{
-		std::cout << "open error" << std::endl;
-	}
+	std::cout << std::format("PART ONE:\r\n\tForwards [{}]\tDowns [{}]\tUps [{}]\r\n\tResult [{}]\r\n", forwards, downs, ups, (downs - ups) * forwards);
 }
 
-void PartTwo()
+void PartTwo(std::vector<DataStruct>* rawData)
 {
 	// example 900
 	// data 1281977850
-	int forwards = 0, ups = 0, downs = 0, aim = 0, depth = 0;
-	std::string dataFileName = "data.txt";
-	std::ifstream dataFileStream(dataFileName);
-	if (dataFileStream.is_open())
+	int forwards = 0, ups = 0, downs = 0, aim = 0, depth = 0, tmpForwards;
+
+	for (auto& data : *rawData)
 	{
-		std::cout << "open ok" << std::endl;
-		std::string line;
-		while (dataFileStream.good())
+		switch (data.wut)
 		{
-			std::getline(dataFileStream, line);
+			case 'f': // forward
+				tmpForwards = data.wat;
+				forwards += tmpForwards;
+				depth += tmpForwards * aim;
+				break;
+			case 'u': // up
+				aim -= data.wat;
+				break;
+			case 'd': // down
+				aim += data.wat;
+				break;
+			default:
+				break;
+		}
+	}
+	std::cout << std::format("PART TWO:\r\n\tForwards [{}]\tAim [{}]\tDepth [{}]\r\n\tResult [{}]\r\n", forwards, aim, depth, forwards * depth);
+}
+
+std::vector<DataStruct> LoadData(std::string fileName)
+{
+	std::vector<DataStruct> r{};
+	std::ifstream dataFile(fileName);
+	if (dataFile.is_open())
+	{
+		std::string line;
+		while (dataFile.good())
+		{
+			std::getline(dataFile, line);
 			switch (line[0])
 			{
-				case 'f': // forward
-				{
-					int f = std::atoi(line.substr(7, line.length() - 7).c_str());
-					forwards += f;
-					depth += f * aim;
-					std::cout << forwards << " " << aim << " " << depth << std::endl;
-				}
-				break;
-				case 'u': // up
-					aim -= std::atoi(line.substr(3, line.length() - 3).c_str());
-					std::cout << "u " << aim << " " << depth << std::endl;
+				case 'f':
+					r.push_back(*(new DataStruct{ 'f', std::stoi(line.substr(7, line.length() - 7)) }));
 					break;
-				case 'd': // down
-					aim += std::atoi(line.substr(4, line.length() - 4).c_str());
-					std::cout << "d " << aim << " " << depth << std::endl;
+				case 'd':
+					r.push_back(*(new DataStruct{ 'd', std::stoi(line.substr(4, line.length() - 4)) }));
+					break;
+				case 'u':
+					r.push_back(*(new DataStruct{ 'u', std::stoi(line.substr(3, line.length() - 3)) }));
 					break;
 				default:
-					std::cout << "da fuq ??" << std::endl;
 					break;
 			}
 		}
-		std::cout << forwards << " - " << aim << " " << depth << " " << forwards * depth << std::endl;
-		dataFileStream.close();
 	}
-	else
-	{
-		std::cout << "open error" << std::endl;
-	}
+	return  r;
 }
 
 int main()
 {
-	//PartOne();
-	PartTwo();
+	std::string fileName = /*"example.txt"*/ "data.txt";
+	std::vector<DataStruct> data = LoadData(fileName);
+	if (data.size() > 0)
+	{
+		PartOne(&data);
+		PartTwo(&data);
+	}
+	else
+	{
+		std::cout << "Can't get data from file [" << fileName << "] for some reason.\r\n";
+	}
 	std::cin.get();
 }
